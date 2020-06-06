65b7375270aacaebb0b3748ae56d2e7d662d101b
deepak.kumar <deepak.rajimr@gmail.com>
2020-06-06 13:45:33 +0530

    Added spring boot jpa

---
+++ my-first-spring-boot-demo/src/test/java/comm/example/ipassion/MyFirstSpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package comm.example.ipassion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstSpringBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ spring-boot-jpa/src/main/java/comm/iris/springbootjpa/SpringBootJpaApplication.java
@@ -0,0 +1,13 @@
+package comm.iris.springbootjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJpaApplication.class, args);
+    }
+
+}
